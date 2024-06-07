6e4c8d84e8233c253518ee40c76d5bdc4526e0c5
prithwish641 <53149922+prithwish641@users.noreply.github.com>
2024-06-07 17:42:57 +0530

    Create fargate.tf

---
+++ modules/aiopsV2/fargate.tf
@@ -0,0 +1,37 @@
+module "eks-fargate-profile-roles" {
+  source  = "./modules/iam_roles"
+  version = ">= 2.0.0"
+  count   = length(var.fargate_profiles) > 0 ? 1 : 0
+
+  account_abbreviation = local.acct_abbreviation
+  name                 = "${var.eks-cluster-name}-fargate-profiles-role"
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Principal = {
+            Service = "eks-fargate-pods.amazonaws.com"
+          },
+          Action = "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+  role_policy_arns = ["arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"]
+}
+
+resource "aws_eks_fargate_profile" "this" {
+  for_each = var.fargate_profiles
+
+  cluster_name           = aws_eks_cluster.eks-cluster.name
+  fargate_profile_name   = "${local.acct_abbreviation}-${data.aws_region.current.name}-eks-fps-${each.key}"
+  pod_execution_role_arn = module.eks-fargate-profile-roles[0].arn
+  selector {
+    namespace = each.value.namespace
+    labels    = try(each.value.labels, null)
+  }
+  subnet_ids = var.subnet_ids
+  tags       = try(each.value.tags, null)
+}
