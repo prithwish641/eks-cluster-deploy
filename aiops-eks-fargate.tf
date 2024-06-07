4beb587255ee7763f2b1722ba2103912905c053f
prithwish641 <53149922+prithwish641@users.noreply.github.com>
2024-06-07 21:57:39 +0530

    Update aiops-eks-fargate.tf

--- aiops-eks-fargate.tf
+++ aiops-eks-fargate.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "aiops-eks-fargate-cluster" {
-  source = ".//modules/aws-eks-cluster/aiopsV2"
+  source = "../../modules/aws-eks-cluster/aiopsV2"
 
   account_abbreviation = "ai"
   prod_non-prod        = "non-prod"
