20cf62035c3d940222cc038231db68508b4d2308
prithwish641 <53149922+prithwish641@users.noreply.github.com>
2024-06-07 21:08:30 +0530

    Update eks_cluster.tf

--- modules/aiopsV2/eks_cluster.tf
+++ modules/aiopsV2/eks_cluster.tf
@@ -1,22 +1,20 @@
 module "eks-cluster-role" {
-  source  = "./modules/iam_roles"
+  source = "./modules/iam_roles"
 
   account_abbreviation = local.acct_abbreviation
   name                 = "${var.eks-cluster-name}-role"
-  assume_role_policy = jsonencode(
-    {
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow"
-          Principal = {
-            Service = "eks.amazonaws.com"
-          },
-          Action = "sts:AssumeRole"
+  assume_role_policy   = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
         }
-      ]
-    }
-  )
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
   role_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
@@ -24,19 +22,22 @@ module "eks-cluster-role" {
   ]
 }
 
-// EKS Service
 resource "aws_eks_cluster" "eks-cluster" {
   name     = "${local.acct_abbreviation}-${data.aws_region.current.name}-${var.eks-cluster-name}"
   role_arn = module.eks-cluster-role.arn
+
   vpc_config {
-    endpoint_private_access = "true"
-    endpoint_public_access  = "true"
+    endpoint_private_access = true
+    endpoint_public_access  = true
     security_group_ids      = [module.eks-security-group-cluster.sg_ids["${local.acct_abbreviation}-${data.aws_region.current.name}-sg-ami-gvt-eks-cluster"]]
     subnet_ids              = var.subnet_ids
   }
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler", ]
-  
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
   version = var.eks_version
+
   depends_on = [
     module.eks-cluster-role
   ]
+}
