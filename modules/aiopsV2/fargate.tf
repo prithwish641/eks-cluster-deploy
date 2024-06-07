ae8610793d5c9e1db32da00c1eedb24de4f14ed5
prithwish641 <53149922+prithwish641@users.noreply.github.com>
2024-06-07 21:00:37 +0530

    Update fargate.tf

--- modules/aiopsV2/fargate.tf
+++ modules/aiopsV2/fargate.tf
@@ -1,6 +1,5 @@
 module "eks-fargate-profile-roles" {
   source  = "./modules/iam_roles"
-  version = ">= 2.0.0"
   count   = length(var.fargate_profiles) > 0 ? 1 : 0
 
   account_abbreviation = local.acct_abbreviation
